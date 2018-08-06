77efe5e80c42153e7b02a36b3ea413bb0cdbd8fd
张涛 <820380768@qq.com>
2018-08-06 23:27:07 +0800

    commit

--- GoodDeed/AppDelegate.m
+++ GoodDeed/AppDelegate.m
@@ -18,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    ///////
     self.window.rootViewController = [GDHomeManager getRootController:NO];
     return YES;
     
