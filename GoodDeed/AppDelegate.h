b2e504e6987d94ac8922e7953c086c0522fbfbed
zhangtao <v_zhangtao05@saicmotor.com>
2020-09-24 22:08:32 +0800

    no message

--- GoodDeed/AppDelegate.h
+++ GoodDeed/AppDelegate.h
@@ -10,7 +10,7 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-@property (strong, nonatomic) UIWindow *testwindow;
+@property (strong, nonatomic) UIWindow *testwindow;///llll
 
 
 @end
